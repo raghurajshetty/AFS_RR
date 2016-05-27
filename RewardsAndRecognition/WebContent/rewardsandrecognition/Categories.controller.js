2a4b412dbd23f959012950a4702538b28f603a77
I064691 <I064691@INLN50870228A.apj.global.corp.sap>
2016-05-27 16:37:38 +0530

    Category Page
    Signed-off-by: I064691 <I064691@INLN50870228A.apj.global.corp.sap>

---
+++ RewardsAndRecognition/WebContent/rewardsandrecognition/Categories.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("rewardsandrecognition.Categories", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf akila_rnr.Akila_DashBoard
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf akila_rnr.Akila_DashBoard
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf akila_rnr.Akila_DashBoard
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf akila_rnr.Akila_DashBoard
+*/
+//	onExit: function() {
+//
+//	}
+
+});
